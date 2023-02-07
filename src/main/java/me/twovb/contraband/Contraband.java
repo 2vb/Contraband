2261edde64e66e3523adab179c4a9971ef21c2c7
2vb <2vb@protonmail.com>
2023-02-06 20:14:14 -0800

    meow

--- src/main/java/me/twovb/contraband/Contraband.java
+++ src/main/java/me/twovb/contraband/Contraband.java
@@ -3,34 +3,36 @@ package me.twovb.contraband;
 import lombok.Getter;
 //import me.twovb.contraband.commands.TestCommand;
 import me.twovb.contraband.listeners.ItemPickupEvent;
+
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Contraband extends JavaPlugin {
 
-    @Getter
-    private static Contraband instance;
+  @Getter
+  private static Contraband instance;
 
-    @Override
-    public void onEnable() {
-        instance = this;
-        this.saveDefaultConfig();
-     //   registerCommands();
-        registerEvents();
-    }
+  @Override
+  public void onEnable() {
+    instance = this;
+    this.saveDefaultConfig();
+    // registerCommands();
+    registerEvents();
+  }
 
-    private void registerEvents() {
-        PluginManager pm = getServer().getPluginManager();
-        pm.registerEvents(new ItemPickupEvent(), this);
-    }
+  private void registerEvents() {
+    PluginManager pm = getServer().getPluginManager();
+    pm.registerEvents(new ItemPickupEvent(), this);
+  }
 
-//    private void registerCommands() {
-  //      getCommand("test").setExecutor(new TestCommand());
-    //    getCommand("test").setTabCompleter(new TestCommand());
-    //}
+  // private void registerCommands() {
+  // getCommand("test").setExecutor(new TestCommand());
+  // getCommand("test").setTabCompleter(new TestCommand());
+  // }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
+  @Override
+  public void onDisable() {
+    // Plugin shutdown logic
+  }
 }
--- src/main/java/me/twovb/contraband/listeners/ItemPickupEvent.java
+++ src/main/java/me/twovb/contraband/listeners/ItemPickupEvent.java
@@ -2,20 +2,24 @@ package me.twovb.contraband.listeners;
 
 import me.twovb.contraband.Contraband;
 import me.twovb.contraband.utils.CC;
-import net.md_5.bungee.api.chat.TextComponent;
 
 import org.bukkit.Material;
-import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class ItemPickupEvent implements Listener {
-    @EventHandler
-    public void onItemPickup(EntityPickupItemEvent event) {
+  @EventHandler
+  public void onItemPickup(EntityPickupItemEvent event) {
     Player player = (Player) event.getEntity();
     Material item = event.getItem().getItemStack().getType();
-    player.sendMessage(String.valueOf(item));
+    // player.sendMessage(String.valueOf(item));
+    for (String droppedItem : Contraband.getInstance().getConfig().getStringList("items")) {
+      if (Material.getMaterial(droppedItem) == item) {
+        player.sendMessage(CC.translate(Contraband.getInstance().getConfig().getString("messages.pickup")));
+      }
+    }
   }
 }
