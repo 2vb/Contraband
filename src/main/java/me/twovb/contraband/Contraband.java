b8d1bba028abf3a890f738ae76e5e69a771f01dd
2vb <2vb@protonmail.com>
2023-03-01 12:05:39 -0800

    work on toggle

--- src/main/java/me/twovb/contraband/Contraband.java
+++ src/main/java/me/twovb/contraband/Contraband.java
@@ -3,9 +3,15 @@ package me.twovb.contraband;
 import lombok.Getter;
 import me.twovb.contraband.commands.ContrabandCommand;
 import me.twovb.contraband.listeners.ItemPickupEvent;
+
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.logging.Level;
 
 public final class Contraband extends JavaPlugin {
@@ -13,12 +19,20 @@ public final class Contraband extends JavaPlugin {
   @Getter
   private static Contraband instance;
 
+  private File itemsFile;
+  private FileConfiguration itemsFileConfig;
+
   @Override
   public void onEnable() {
     instance = this;
     this.saveDefaultConfig();
     registerCommands();
     registerEvents();
+    createItemsFile();
+  }
+
+  public FileConfiguration getItems() {
+    return this.itemsFileConfig;
   }
 
   private void registerEvents() {
@@ -29,6 +43,21 @@ public final class Contraband extends JavaPlugin {
   private void registerCommands() {
     getCommand("contraband").setExecutor(new ContrabandCommand());
     getCommand("contraband").setTabCompleter(new ContrabandCommand());
+  }
+
+  private void createItemsFile() {
+    itemsFile = new File(getDataFolder(), "items.yml");
+    if (!itemsFile.exists()) {
+      itemsFile.getParentFile().mkdirs();
+      saveResource("items.yml", false);
+    }
+    try {
+      itemsFileConfig = new YamlConfiguration();
+      itemsFileConfig.load(itemsFile);
+    } catch (IOException | InvalidConfigurationException e) {
+      e.printStackTrace();
+
+    }
   }
 
   @Override
